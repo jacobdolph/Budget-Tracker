87f7d24643d9ee87ed9ba407a8a6bf391b6258fa
Jacob Dolph <jacob.dolph702@gmail.com>
2020-03-30 20:14:03 -0600

    initial commit, set up my webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const config = {
+    entry: "./server.js",
+    output: {
+        path: __dirname + "/dist",
+        filename: "bundle.js"
+    },
+    mode: "development",
+    plugins: [
+        new WebpackPwaManifest({
+            filename: "manifest.json",
+            inject: false,
+            fingerprints: false,
+            name: "Budget App",
+            short_name: "Budjet App",
+            theme_color: "#ffffff",
+            background_color: "#ffffff",
+            start_url: "/",
+            display: "standalone",
+
+            icons: [
+                {
+                    src: path.resolve(
+                        __dirname,
+                        "./public/icons/icon-512x512.png"
+                    ),
+                    size: [72, 96, 128, 144, 152, 192, 384, 512]
+                }
+            ]
+        })
+    ]
+}
+
+module.exports = config;
