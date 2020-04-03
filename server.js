3119e8aae3d0f8ba44262593dfebd2170f90c7f7
Jacob Dolph <jacob.dolph702@gmail.com>
2020-04-03 09:02:21 -0600

    changed environment variables for deployment to heroku

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
