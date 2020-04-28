46188737c23ac78c2eec510b429fe0508f8a15a3
Jacob Dolph <jacob.dolph702@gmail.com>
2020-04-28 17:05:21 -0600

    created bootstrap buttons and inputs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const compression = require("compression");
+const express = require('express')
+const logger = require('morgan')
+const mongoose = require('mongoose')
+const compression = require('compression')
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
